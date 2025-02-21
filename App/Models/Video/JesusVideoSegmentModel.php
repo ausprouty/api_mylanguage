536bdb1949b1e93481cc86577c0da300bd7b72ca
ausprouty2 <bob.prouty@powertochange.org.au>
2025-02-21 19:36:36 +1100

    restored from backups

--- App/Models/Video/JesusVideoSegmentModel.php
+++ App/Models/Video/JesusVideoSegmentModel.php
@@ -2,26 +2,140 @@
 
 namespace App\Models\Video;
 
-use App\Services\Database\DatabaseService;
 
-class JesusVideoSegmentModel
- {
-    private $databaseService;
-    private $id;
-    private $title;
-    private $verses;
-    private $videoSegment;
-    private $stopTime;
-   
+use App\Interfaces\ArclightVideoInterface;
+use ReflectionClass;
 
-    public function __construct(DatabaseService $databaseService) {
-        $this->databaseService = $databaseService;
-        $this->id = '';
+class JesusVideoSegmentModel implements ArclightVideoInterface{
+
+    private int $id;
+    private string $title;
+    private string $verses;
+    private string $videoSource;
+    private string $videoPrefix;
+    private string $videoCode;
+    private string $videoSegment;
+    private string $startTime;
+    private string $stopTime;
+
+    public function __construct() {
+
+        $this->id = 0;
         $this->title = '';
         $this->verses = '';
-        $this->videoSegment= '';
-        $this->stopTime= '';
+        $this->videoSource = '';
+        $this->videoPrefix = '';
+        $this->videoCode = '';
+        $this->videoSegment = '';
+        $this->startTime = 0;
+        $this->stopTime = '';
+    }
+     // Method to populate the model
+     public function populateFromArray(array $data): void {
+        $this->id = $data['id'] ?? 0;
+        $this->title = $data['title'] ?? '';
+        $this->verses = $data['verses'] ?? '';
+        $this->videoSource = $data['videoSource'] ?? '';
+        $this->videoPrefix = $data['videoPrefix'] ?? '';
+        $this->videoCode = $data['videoCode'] ?? '';
+        $this->videoSegment = $data['videoSegment'] ?? '';
+        $this->startTime = $data['startTime'] ?? 0;
+        $this->stopTime = $data['stopTime'] ?? '';
     }
 
-}
+    // Getters
 
+     /**
+     * Returns the video properties as an associative array.
+     *
+     * @return array
+     */
+    public function getProperties(): array
+    {
+        $reflection = new ReflectionClass($this);
+        $properties = $reflection->getProperties();
+        $propsArray = [];
+
+        foreach ($properties as $property) {
+            $property->setAccessible(true); // Allows access to private property
+            $propsArray[$property->getName()] = $property->getValue($this);
+        }
+
+        return $propsArray;
+    }
+
+    public function getId(): int {
+        return $this->id;
+    }
+
+    public function getTitle(): string {
+        return $this->title;
+    }
+
+    public function getVerses(): string {
+        return $this->verses;
+    }
+
+    public function getVideoSource(): string {
+        return $this->videoSource;
+    }
+
+    public function getVideoPrefix(): string {
+        return $this->videoPrefix;
+    }
+
+    public function getVideoCode(): string {
+        return $this->videoCode;
+    }
+
+    public function getVideoSegment(): string {
+        return $this->videoSegment;
+    }
+    public function getStartTime(): string {
+        return $this->startTime;
+    }
+    public function getEndTime(): string {
+        return $this->startTime;
+    }
+
+
+    public function getStopTime(): string {
+        return $this->stopTime;
+    }
+
+    // Setters
+    public function setId(int $id): void {
+        $this->id = $id;
+    }
+
+    public function setTitle(string $title): void {
+        $this->title = $title;
+    }
+
+    public function setVerses(string $verses): void {
+        $this->verses = $verses;
+    }
+
+    public function setVideoSource(string $videoSource): void {
+        $this->videoSource = $videoSource;
+    }
+
+    public function setVideoPrefix(string $videoPrefix): void {
+        $this->videoPrefix = $videoPrefix;
+    }
+
+    public function setVideoCode(string $videoCode): void {
+        $this->videoCode = $videoCode;
+    }
+
+    public function setVideoSegment(string $videoSegment): void {
+        $this->videoSegment = $videoSegment;
+    }
+    public function setStartTime(string $startTime): void {
+        $this->stopTime = $startTime;
+    }
+
+    public function setStopTime(string $stopTime): void {
+        $this->stopTime = $stopTime;
+    }
+}
--- App/Services/VideoService.php
+++ App/Services/VideoService.php
@@ -5,6 +5,8 @@ namespace App\Services;
 use App\Services\TwigService;
 use App\Configuration\Config;
 use App\Models\Bible\PassageReferenceModel;
+use App\Interfaces\ArclightVideoInterface;
+use App\Helpers\TimeHelper;
 
 class VideoService {
 
@@ -23,11 +25,10 @@ class VideoService {
 
         // Ensure TwigService s a render method
         $output = $this->twigService->render($template, $videoInfo);
-        print_r($output);
-        die;
+      
     }
 
-    public function getUrl(
+    public function getUrlFromPassageReference(
         PassageReferenceModel $passageReferenceInfo, 
         string $languageCodeJF){
         
@@ -37,59 +38,41 @@ class VideoService {
 
     }
 
-    private function getArclightUrl(
-        PassageReferenceModel $passageReferenceInfo, 
-        string $languageCodeJF): string {
-        if (!$languageCodeJF){
-            $url = null;
-            return $url;
+    public static function getArclightUrl(
+        ArclightVideoInterface $videoInfo, 
+        string $languageCodeJF
+    ): ?string {
+     
+        if (!$languageCodeJF || $videoInfo->getVideoSource() !== 'arclight') {
+            return null;
         }
-        if ($passageReferenceInfo->getVideoSource() !== 'arclight'){
-            $url = null;
-            return $url;
-        }
+
         $url = Config::get('api.jvideo_player');
-        $url .= $passageReferenceInfo->getVideoPrefix();
+        $url .= $videoInfo->getVideoPrefix();
         $url .= $languageCodeJF;
-        $url .= $passageReferenceInfo->getVideoCode();
-      
-        $url .= $passageReferenceInfo->getVideoSegment();
-        if ($passageReferenceInfo->getEndTime()){
-            $seconds = $this->convertMinutesToSeconds($passageReferenceInfo->getStartTime());
-            $url .= '&start=' . $seconds;
-            $seconds =  $this->convertMinutesToSeconds($passageReferenceInfo->getEndTime());
-            $url .= '&end=' . $seconds ;
+        $url .= $videoInfo->getVideoCode();
+        $url .= $videoInfo->getVideoSegment();
+
+        if ($videoInfo->getEndTime()) {
+            $startSeconds = TimeHelper::convertMinutesToSeconds($videoInfo->getStartTime());
+            $url .= '&start=' . $startSeconds;
+            $endSeconds = TimeHelper::convertMinutesToSeconds($videoInfo->getEndTime());
+            $url .= '&end=' . $endSeconds;
         }
+
         $url .= '&playerStyle=default';
         return $url;
     }
 
-    private function computeParameters(array $translation){
+
+    protected function computeParameters(array $translation){
         $videoInfo = [];
         $videoInfo['videoCode'] = $translation['videoCode'] ?? '';
         $videoInfo['url'] =  $videoInfo['videoCode'];
         $videoInfo['videoSegment'] =  $translation['videoSegment'];
-        $videoInfo['startTime'] = $this->convertMinutesToSeconds($translation['startTime'] ?? '0:00');
-        $videoInfo['endTime'] = $this->convertMinutesToSeconds($translation['endTime'] ?? '0:00');
+        $videoInfo['startTime'] = TimeHelper::convertMinutesToSeconds($translation['startTime'] ?? '0:00');
+        $videoInfo['endTime'] = TimeHelper::convertMinutesToSeconds($translation['endTime'] ?? '0:00');
         return $videoInfo;
     }
 
-    // Function to convert time to seconds
-    public function convertMinutesToSeconds(string $time): int {
-        // Handle invalid input gracefully
-        // Some videos use 'start' which should be replaced with 0
-        if (!str_contains($time, ':')) {
-            return 0;
-        }
-
-        // Split the input string into minutes and seconds
-        list($minutes, $seconds) = explode(':', $time);
-
-        // Convert minutes and seconds to integers
-        $minutes = (int) $minutes;
-        $seconds = (int) $seconds;
-
-        // Calculate total seconds
-        return ($minutes * 60) + $seconds;
-    }
-}
+}    
